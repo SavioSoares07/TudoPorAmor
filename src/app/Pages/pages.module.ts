13a4491328ca8dd487d926e8b4c4c91863759686
Savio Soares <dsaviosnogueira@gmail.com>
2024-09-25 16:35:36 -0300

    Add more cards

--- src/app/Pages/pages.module.ts
+++ src/app/Pages/pages.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { AngularMaterial } from '../angular-material/angular-material.module';
 import { Components } from '../components/components.module';
 import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, DetailsComponent],
   imports: [AngularMaterial, Components],
   exports: [HomeComponent],
 })
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Routes
+import { HomeComponent } from './Pages/home/home.component';
+import { DetailsComponent } from './Pages/details/details.component';
+import path from 'path';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'detalhes', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
