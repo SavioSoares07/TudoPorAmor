952e0c4e306a15ec32d78434a714206e1740cb53
Savio Soares <dsaviosnogueira@gmail.com>
2024-10-01 16:26:46 -0300

    Create page vaccine

--- src/app/Pages/pages.module.ts
+++ src/app/Pages/pages.module.ts
@@ -4,10 +4,11 @@ import { Components } from '../components/components.module';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 
-import { RouterModule } from '@angular/router'; // Importando o RouterModule
+import { RouterModule } from '@angular/router';
+import { VaccineComponent } from './vaccine/vaccine.component'; // Importando o RouterModule
 
 @NgModule({
-  declarations: [HomeComponent, DetailsComponent],
+  declarations: [HomeComponent, DetailsComponent, VaccineComponent],
   imports: [AngularMaterial, Components, RouterModule],
   exports: [HomeComponent],
 })
---
+++ src/app/Pages/vaccine/vaccine.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vaccine',
+  templateUrl: './vaccine.component.html',
+  styleUrl: './vaccine.component.scss'
+})
+export class VaccineComponent {
+
+}
