fbcb22d92c9ce3ce8bcac7a1a58255eb91e1a812
Savio Soares <dsaviosnogueira@gmail.com>
2024-10-07 12:48:23 -0300

    Create pages register and login

--- src/app/Pages/pages.module.ts
+++ src/app/Pages/pages.module.ts
@@ -6,10 +6,12 @@ import { DetailsComponent } from './details/details.component';
 import { RouterModule } from '@angular/router';
 import { VaccineComponent } from './vaccine/vaccine.component';
 import { PrimeNg } from '../primeng/primeng.module';
-import { FormsModule } from '@angular/forms'; // Importando FormsModule
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component'; // Importando FormsModule
 
 @NgModule({
-  declarations: [HomeComponent, DetailsComponent, VaccineComponent],
+  declarations: [HomeComponent, DetailsComponent, VaccineComponent, RegisterComponent, LoginComponent],
   imports: [AngularMaterial, Components, RouterModule, PrimeNg, FormsModule], // Adicionando FormsModule
   exports: [HomeComponent, DetailsComponent, VaccineComponent],
 })
